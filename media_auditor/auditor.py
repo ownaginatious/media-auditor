0d45a38f67877aecb43fd8e9ac94ac69c8d166ac
Dillon Dixon <dillondixon@gmail.com>
2015-12-08 18:10:38 -0800

    Made the media auditor into a class with a generator for locating media files

--- media_auditor/auditor.py
+++ media_auditor/auditor.py
@@ -1,28 +1,50 @@
 import json
 import os
 import subprocess
+import logging
+import sys
+import tempfile
 
-NOT_MEDIA = "Invalid data found when processing input"
+class MediaAuditor(object):
 
-for d, subdirs, files in os.walk("."):
-    for f in files:
-        f = d + "/" + f
+    LOGGER = logging.getLogger(__name__)
+    NOT_MEDIA = "Invalid data found when processing input"
 
-        # Don't check non-regular files (e.g. socket/device handles)
-        if not os.path.isfile(f):
-            continue
+    def find_media_files(self):
 
-        p = subprocess.Popen(['ffprobe', '-show_streams', '-print_format', 'json', f],\
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        code = p.wait()
-        stdout, stderr = p.communicate()
-        stdout, stderr = stdout.decode("utf-8"), stderr.decode("utf-8")
-        if code == 0:
-            print("Data discovered for: '%s'" % f)
-            data = json.loads(stdout)
-            for num, data in enumerate(data['streams']):
-                print('%s: %s' % (num, data['codec_name']))
-        elif NOT_MEDIA in stderr:
-            print("File '%s' is not a media file" % f)
-        else:
-            print("Error reading '%s': %s" % (f, stderr))
+        for d, subdirs, files in os.walk('/Users/dillon/Dropbox'):
+            for f in files:
+                f = d + "/" + f
+
+                # Don't check non-regular files (e.g. socket/device handles)
+                if not os.path.isfile(f):
+                    continue
+
+                with tempfile.TemporaryFile() as stdout, tempfile.TemporaryFile() as stderr:
+
+                    p = subprocess.Popen(['ffprobe', '-show_streams', '-print_format', 'json', f],
+                            stdout=stdout,
+                            stderr=stderr)
+
+                    code = p.wait()
+
+                    stdout.seek(0)
+                    stderr.seek(0)
+
+                    out = stdout.read().decode('utf-8')
+                    err = stderr.read().decode('utf-8')
+
+                    if code == 0:
+                        data = json.loads(out)
+                        data['path'] = f
+                        yield data
+                    elif self.NOT_MEDIA in err:
+                        self.LOGGER.debug("The file is not a media file '{}'".format(f))
+                    else:
+                       self.LOGGER.warn("Error probing '{}': {}...".format(f, err[0:30]))
+
+logger = logging.getLogger(MediaAuditor.__name__)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+
+ma = MediaAuditor()
+print("Found {} media files".format(len([x for x in ma.find_media_files()])))
